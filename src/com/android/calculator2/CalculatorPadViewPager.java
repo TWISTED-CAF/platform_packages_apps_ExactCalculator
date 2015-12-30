3edfb53014bd7fcbbfe830b79ffb2a439cb6e7b8
Bryan Owens <djbryan3540@gmail.com>
2015-12-29 22:25:10 -0600

    Themes: Expose hard coded background in java
    Change-Id: I30bade7c82f29767c6659c8a75bb402d245e49f1
    
    Conflicts:
    	src/com/android/calculator2/CalculatorPadViewPager.java

--- src/com/android/calculator2/CalculatorPadViewPager.java
+++ src/com/android/calculator2/CalculatorPadViewPager.java
@@ -98,8 +98,8 @@ public class CalculatorPadViewPager extends ViewPager {
         super(context, attrs);
 
         setAdapter(mStaticPagerAdapter);
-        setBackgroundColor(getResources().getColor(android.R.color.black));
         setOnPageChangeListener(mOnPageChangeListener);
+        setBackgroundColor(context.getColor(R.color.pad_view_pager_background));
         setPageMargin(getResources().getDimensionPixelSize(R.dimen.pad_page_margin));
         setPageTransformer(false, mPageTransformer);
     }
